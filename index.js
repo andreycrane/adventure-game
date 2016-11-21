c7e9ed52f39212222c49e6a58989e645e8b9627a
Andrey Crane <crane.andrey@gmail.com>
2016-11-21 21:32:20 +0300

    Добавил файлы для хостинга на heroku

---
+++ index.js
@@ -0,0 +1,54 @@
+const 
+	http = require('http'),
+	fs = require('fs'),
+	path = require('path');
+
+http.createServer((req, res) => {
+	let filePath = '.' + req.url;
+	
+	if ( filePath == './' ) {
+		filePath = './index.html';
+	}
+	
+	const extname = path.extname(filePath);
+	let contentType = 'text/html';
+	
+	switch (extname) {
+		case '.js':
+			contentType = 'text/javascript';
+			break;
+		case '.css':
+			contentType = 'text/css';
+			break;
+		case '.json':
+			contentType = 'application/json';
+			break;
+		case '.png':
+			contentType = 'image/png';
+			break;
+		case '.jpg':
+			contentType = 'image/jpg';
+			break;
+		case '.wav':
+			contentType = 'audio/wav';
+			break;
+	}
+	
+	fs.readFile(filePath, (error, content) => {
+		if (error) {
+			if(error.code == 'ENOENT'){
+				fs.readFile('./404.html', (error, content) => {
+					res.writeHead(200, { 'Content-Type': contentType });
+					res.end(content, 'utf-8');
+				});
+			} else {
+				res.writeHead(500);
+				res.end(`Sorry, check with the site admin for error: ${error.code}`);
+				res.end(); 
+			}
+		} else {
+			res.writeHead(200, { 'Content-Type': contentType });
+			res.end(content, 'utf-8');
+		}
+	});
+}).listen(process.env.PORT || 8000);
