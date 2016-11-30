b8508b517796556b870215b8be312435d7015991
Andrey Crane <crane.andrey@gmail.com>
2016-12-01 01:37:29 +0300

    Укачивает его, афигет!

--- js/game.js
+++ js/game.js
@@ -153,10 +153,6 @@ Adventure.Game.prototype = {
 		this.o.dec1.tilePosition.x = -this.game.camera.x * 0.025;
 		this.o.dec2.tilePosition.x = -this.game.camera.x * 0.05;
 		this.o.dec3.tilePosition.x = -this.game.camera.x * 0.1;
-		
-		this.o.dec1.y = this.game.camera.y + this.game.camera.height - this.o.dec1.height;
-		this.o.dec2.y = this.game.camera.y + this.game.camera.height - this.o.dec2.height;
-		this.o.dec3.y = this.game.camera.y + this.game.camera.height - this.o.dec3.height;
 	},
 	
 	update: function() {
